747a0dddde937537157fc855c07d2be869d5270a
Bxtohur <bitohur@gmail.com>
2023-12-11 08:45:25 +0700

    Add FullScreen

--- app/src/main/java/com/bitohur/exoplayer/MainActivity.kt
+++ app/src/main/java/com/bitohur/exoplayer/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.bitohur.exoplayer
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import com.bitohur.exoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,16 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val fullScreenButtonClickListener =
+        PlayerView.FullscreenButtonClickListener { isFullScreen ->
+            requestedOrientation = if (isFullScreen) {
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            } else {
+                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            }
+        }
+
+
     @OptIn(UnstableApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +34,9 @@ class MainActivity : AppCompatActivity() {
         val player = ExoPlayer.Builder(this).build()
         val videoUri = getString(R.string.link_video)
         val playerView = binding.playerView
+
+        playerView.setFullscreenButtonClickListener(fullScreenButtonClickListener)
+
         playerView.player = player
         val mediaItem = MediaItem.fromUri(videoUri)
         player.setMediaItem(mediaItem)
